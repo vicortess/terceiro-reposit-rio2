6cf4f751b3bec8f01cc317781e79af622d5369b3
vicortess <149524861+vicortess@users.noreply.github.com>
2025-06-23 08:53:09 -0300

    main novo

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const numeroSenha= document.querySelector('.parametro-senha__texto');
+const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
@@ -7,32 +7,32 @@ const botoes = document.querySelectorAll('.parametro-senha__botao');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho(){
-    if (tamanhoSenha >1) {
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
 
-function aumentaTamanho(){
-    if (tamanhoSenha <20){
+function aumentaTamanho() {
+    if (tamanhoSenha < 20){
         tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
 
-const campoSenha= document.querySelector('#campo-senha');
-const letrasMaiuscula = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
 
 function geraSenha() {
     let senha = '' ;
-    for(let i=0 < tamanhoSenha; i++) {
+    for(let i=0; i < tamanhoSenha; i++) {
         let numeroAleatorio = Math.random()*letrasMaiusculas.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
         senha = senha + letrasMaiusculas[numeroAleatorio];
     }
-    campoSenha.value = letrasMaiusculas;
+    campoSenha.value = letrasMaiusculas; 
 }
