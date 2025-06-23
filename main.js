72814781048adb4a4f9ac11d0535bea6e7f1550d
vicortess <149524861+vicortess@users.noreply.github.com>
2025-06-23 08:44:46 -0300

    23-06

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function aumentaTamanho(){
 }
 
 const campoSenha= document.querySelector('#campo-senha');
-const letrasMaiuscula= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMaiuscula = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 
 function geraSenha() {
